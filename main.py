7fd3f10642c69c90eab57b05f60c01a88e9233c6
Tarini Bengani <tarinibengani@Tarinis-MacBook-Pro-2.local>
2024-11-28 23:39:32 -0500

    first function written

---
+++ main.py
@@ -0,0 +1,8 @@
+
+def user_input():
+    company_list = []
+    for i in range(4):
+        company_name = input("Enter a company name: ")
+        company_list.append(company_name)
+
+    return company_list
